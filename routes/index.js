98464893f75bde3e66d38b565abd2985046c6468
Eric Sheris <esheris@live.com>
2016-04-20 23:43:09 -0700

    most api done, working on ui

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+function get_allvideos(req, callback) {
+    var options = {
+        uri : "http://" + req.get('host') + '/api/vidsfromfolder',
+        method : 'GET'
+    };
+    var res = '';
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res = body;
+        }
+        else {
+            res = 'Not Found';
+        }
+        callback(res);
+    });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  get_allvideos(req,function(resp){
+    console.log(resp)
+    res.render('index',{videos: resp})
+  })
 });
 
 module.exports = router;
--- routes/videos.js
+++ routes/videos.js
@@ -2,27 +2,65 @@ var express = require('express');
 var sqllite3 = require('sqlite3');
 var db = new sqllite3.Database("./data/videoqueue.db")
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 //https://github.com/mapbox/node-sqlite3/wiki/API
 /* GET users listing. */
 router.get('/nextvideo', function(req, res, next) {
   db.get("SELECT rowid, video_name from video_queue where played = 'false'",function(err,row){
-    /*db.run("Delete from video_queue where rowid = $rowid",{
-      $rowid: row.rowid
-    })*/
-    console.log(row);
-    res.send(row);
-  })
-  
+    if (row){
+      db.run("UPDATE video_queue SET played = 'true' where rowid = $rowid",{
+        $rowid: row.rowid
+      })
+      res.send(row);
+    } else {
+      res.send();
+    }
+  });
 });
 
-router.post('/nextvideo',function(req,res,next){
-  console.log("req");
+router.get('/allvideo',function(req,res,next){
+  db.all("Select rowid as id, * from video_queue",function(err,row){
+    if (row){
+      res.send(row);
+    } else {
+      res.send("No Results")
+    }
+  })
+})
+
+function fileList(dir) {
+  return fs.readdir(dir, function(err,items){
+    console.log(items)
+    return items
+  })
+}
+
+router.get('/vidsfromfolder', function(req,res,next){
+  res.send(fileList('./public/videos'))
+})
+
+router.post('/nextvideo',function(req,res, next){
   db.run("INSERT INTO video_queue(video_name, played) VALUES ($video_name, $played)",{
     $video_name: req.body.video_name,
     $played: "false"
   },function(dbres){
-    res.send(dbres)
+    res.send(dbres);
   });
-})
+});
+
+router.param('rowid',function(req,res,next,rowid){
+  req.rowid = rowid;
+  next();
+});
+
+router.delete('/nextvideo/:rowid', function(req,res, next){
+  console.log("delete " + req.params.rowid);
+  db.run("DELETE from video_queue where rowid = $rowid",{
+    $rowid: req.params.rowid
+  },function(dbres){
+    res.send(dbres);
+  });
+});
 
 module.exports = router;
