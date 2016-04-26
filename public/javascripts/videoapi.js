45cc2df9d4559d2e8d5f1f134b77aa3444aa3dc1
Eric Sheris <esheris@live.com>
2016-04-26 08:37:31 -0700

    broke js

--- public/javascripts/videoapi.js
+++ public/javascripts/videoapi.js
@@ -1,10 +1,20 @@
 function postvideo(url){
-        $.ajax({
-          type: "POST",
-          url: "/api/nextvideo",
-          data: {video_name: url},
-          success: function(){
-            $('#message').html("Success")
-          },
-        })
+  $.ajax({
+    type: "POST",
+    url: "/api/nextvideo",
+    data: {video_name: url},
+    success: function(){
+      $('#message').html("Success")
+    },
+  })
+}
+
+function poll(){
+  $.ajax({ url: "/api/nextvideo", success: function(data) {
+      if (data){
+          $('#display').html = "<video><source src=" + data.video_name +</video>
+      } else {
+        $('#display').html = ""
       }
+  } });
+}
