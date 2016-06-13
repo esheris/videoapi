918f1be796781342d1711f31e15fe332d254e394
Eric Sheris <esheris@live.com>
2016-06-13 11:24:17 -0700

    chuck norris

--- Bot/irc.js
+++ Bot/irc.js
@@ -2,7 +2,8 @@ var irc = require("tmi.js")
 var request = require ('request')
 
 var mychannel = "#phantasaia"
-var cmdtimeout = 120000;
+var emotetimeout = 120000;
+var cmdTimeout = 30000;
 
 var options = {
   options: {
@@ -22,15 +23,18 @@ var options = {
 var client = new irc.client(options);
 var rekt = 0
 var rektLastCalled = Date.now();
+var chuckLastCall = Date.now()
 client.connect()
 client.on("chat", function(channel, user, message, self){
   if (message.includes("phanRekt")){
     rekt ++;
-    console.log('Rekt = ' + rekt);
   }
-  if ((rekt >= 3) && ((Date.now() - rektLastCalled) > cmdtimeout)){
+  if ((rekt >= 3) && ((Date.now() - rektLastCalled) > emotetimeout)){
     postvideo('/media/somebodysbitch.mp4')
     rekt = 0;
+  }
+  if ((message.startsWith("!chuck")) && ((Date.now() - chuckLastCall) > cmdTimeout)){
+    getchuckJoke()
   }
 })
 
@@ -42,4 +46,12 @@ function postvideo(url){
       }
     }
   )
+}
+
+function getchuckJoke(){
+  request.get("https://api.chucknorris.io/jokes/random",function(error,response,body){
+    if (!error && response.statusCode == 200){
+      client.say(mychannel, JSON.parse(body).value)
+    }
+  })
 }
